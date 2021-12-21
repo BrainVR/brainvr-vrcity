9f5c08b8c796a9d845bc0e4a0aa13207f8853183
Lukáš Hejtmánek <hejtmy@gmail.com>
2021-12-21 17:58:27 +0100

    Updates package with basic folder searches

---
+++ R/vrcity-file-operations.R
@@ -0,0 +1,27 @@
+#' Returns paths to tasks of given name in given session folder
+#'
+#' @param folder session forlder in which to search
+#' @param taskname name of the task to search for
+#'
+#' @return paths to folders with a given task
+#' @export
+#'
+#' @examples
+search_task <- function(folder, taskname){
+  ptr <- paste0(".*-", taskname, collapse = "")
+  dirs <- list.dirs(folder, recursive = FALSE)
+  dirs <- dirs[grepl(ptr, dirs)]
+  return(dirs)
+}
+
+#' Title
+#'
+#' @param folder
+#'
+#' @return
+#' @export
+#'
+#' @examples
+list_tasks <- function(folder){
+
+}
