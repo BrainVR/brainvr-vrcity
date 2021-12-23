fc493518f95cdd2d5bbbd1d1d2cf11c574431cd7
Lukáš Hejtmánek <hejtmy@gmail.com>
2021-12-23 01:53:48 +0100

    Adds automatic processing of position data

--- R/vrcity-preprocessing.R
+++ R/vrcity-preprocessing.R
@@ -1,10 +1,12 @@
 process_log <- function(ls_log){
-  if(!is.null(ls_log[["data"]])){
-    ls_log$data <- trim_data_frame(ls_log$data)
-  }
+  if(is.null(ls_log[["data"]])) return(ls_log)
+  ls_log$data <- trim_data_frame(ls_log$data)
+  # conversts vectors to numbers and flips Z and Y
+  ls_log$data <- convert_vector3(ls_log$data)
   return(ls_log)
 }
-#' Title
+
+#' Trims leading space from the string values
 #'
 #' @param data.frame
 #'
@@ -17,3 +19,25 @@ trim_data_frame <- function(df){
   df <- mutate(df, across(where(is.character), str_trim))
   return(df)
 }
+
+#' THIS CONVECRTS Z and Y axes
+#' @importFrom brainvr.reader
+convert_vector3 <- function(df){
+  is_unity_vector <- function(text){
+    if(!is.character(text)) return(FALSE)
+    # This is kinda garbage, but quick and it works
+    ptr <- "\\([-]*\\d+.*\\d+.*\\d+.*\\)"
+    return(grepl(ptr, text))
+  }
+  # get columns
+  vector_columns <- colnames(df)[sapply(df, function(x){is_unity_vector(x[1])})]
+  for(col in vector_columns){
+    df[[col]] <- gsub("[\\(\\)]", "", df[[col]])
+    df <- tidyr::separate(df, col, into = paste(col, c("x", "z", "y"), sep="_"),
+                          convert = TRUE, sep=",")
+  }
+  return(df)
+}
+
+
+
