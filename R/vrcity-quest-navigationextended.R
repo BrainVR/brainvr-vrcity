564c79435f5d225f552f69321c8be930097d2278
Lukáš Hejtmánek <hejtmy@gmail.com>
2022-01-17 02:05:19 +0100

    Adds navigaiton getting targets

--- R/vrcity-quest-navigationextended.R
+++ R/vrcity-quest-navigationextended.R
@@ -15,3 +15,34 @@ analyse_task_performance.citynavigation <- function(task){
   )
   return(out)
 }
+
+#' Returns english code for a navigation target
+#'
+#' @param task
+#'
+#' @return
+#' @export
+#'
+#' @examples
+get_navigation_target <- function(task){
+  target <- tolower(gsub("CITY_MAIN/", "", task$test$questconfig_header$navigationPoint))
+  return(target)
+}
+
+#' Returns x, y position of a target site
+#'
+#' @param task
+#'
+#' @return
+#' @export
+#'
+#' @examples
+get_navigation_target_position <- function(task){
+  target <- get_navigation_target(task)
+  location <- LOCATIONS[LOCATIONS$code == target, ]
+  if(nrow(location) < 1){
+    warning("There is no location of name ", target, " in the location")
+    return(NULL)
+  }
+  return(c(location$x, location$z))
+}
